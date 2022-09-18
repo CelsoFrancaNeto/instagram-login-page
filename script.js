1bdfc60deab4f6e0dcb4e9dcf0225de014ad8cc8
CelsoFrancaNeto <celso.franca.neto@gmail.com>
2022-09-18 15:59:36 -0300

    Show password button modified

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
+function showButtonPassword() {
+    var showPasswordButton = document.getElementById("show-button");
+    var passwordInput = document.getElementById("password");
+    showPasswordButton.style.display = "flex";
+    passwordInput.style.borderRight = "none";
+
+}
 function showPassword() {
     var password = document.getElementById("password");
     var showMensage = document.getElementById("show-mensage");
-    if(password.type == "password") {
-        password.type="text";
+    if (password.type == "password") {
+        password.type = "text";
         showMensage.textContent = "Ocultar";
     } else {
         password.type = "password";
@@ -18,6 +25,15 @@ function showButtonBorderFocus() {
 
 function showButtonBorderBlur() {
     var button = document.getElementById("show-button");
+    const passwordInput = document.querySelector("#password");
+    var showPasswordButton = document.getElementById("show-button");
+
     button.style.border = "1px solid #dbdbdb";
     button.style.borderLeft = "none";
+
+    if (passwordInput.value == "") {
+        showPasswordButton.style.display = "none";
+        passwordInput.style.borderRight = "1px solid #dbdbdb";
+    }
+
 }
